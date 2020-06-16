f3b321150bd20c1b77361156a3bb48d24d5d21ec
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-06-16 13:11:39 -0300

    Backend: Parameter Types (either Query, Route or Request Body)

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+/* Api Mestra */
 app.get('/', (request, response) => {
     return response.json({ message: "Hello, GoStack!" });
 });
 
+/* Api de Projetos */
 app.get('/projects', (request, response) => {
+    const query = request.query;
+    console.log(query);
+
     return response.json([
         {id: 1, name: "Projeto 1"},
         {id: 2, name: "Projeto 2"},
@@ -15,6 +22,9 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+    const body = request.body;
+    console.log(body);
+
     return response.json([
         {id: 1, name: "Projeto 1"},
         {id: 2, name: "Projeto 2"},
@@ -24,6 +34,9 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+    const {id} = request.params;
+    console.log(id);
+
     return response.json([
         {id: 1, name: "Projeto 4"},
         {id: 2, name: "Projeto 2"},
@@ -32,12 +45,16 @@ app.put('/projects/:id', (request, response) => {
 });
 
 app.delete('/projects/:id', (request, response) => {
+    const {id} = request.params;
+    console.log(id);
+
     return response.json([
         {id: 2, name: "Projeto 2"},
         {id: 3, name: "Projeto 3"},
     ]);
 });
 
+/* Console message that tells us that this app has started */
 app.listen(3333, () => {
     console.log("🚀 Backend has started!")
 });
